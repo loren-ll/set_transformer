5a8d195ea61265283c277d11a4e4d1cefcb9d683
loren-ll <163414650+loren-ll@users.noreply.github.com>
2025-11-30 12:24:59 +0100

    Add working setup.py for pip installation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="set_transformer",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=["set_transformer", "set_transformer.*"]),
     install_requires=[
         "torch",
     ],
